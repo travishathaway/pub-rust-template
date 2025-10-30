d11f5a34a6eb5edd535b29abebcbbb1af18529b2
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-10-30 18:13:01 +0100

    adding a few things

--- math_demo/__main__.py
+++ math_demo/__main__.py
@@ -1,6 +1,9 @@
 from cowpy.cow import Cowacter
 
-from .math_demo import add
+from .math_demo import add, hello
 
 message = Cowacter().milk(f"3 + 7 = {add(3, 7)}")
 print(message)
+
+print()
+hello("PUB")
